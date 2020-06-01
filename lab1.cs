ca5e68cac4f6654bf084783b12e2aebb7f53e1cc
Rytenz <38050661+Rytenz@users.noreply.github.com>
2020-06-01 18:17:57 +0300

    Add files via upload

---
+++ lab1.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System;
+namespace Helloworld { 
+    class Hello { static void Main() 
+        { Console.WriteLine("Hey!"); 
+            Console.WriteLine("Press any key to quit"); 
+            Console.ReadKey(); } } }
